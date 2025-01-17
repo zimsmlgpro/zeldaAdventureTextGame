f0edeaa1b7c593b1f003d739bccf264179a624bd
Brandon Dennis <bdennis@ad.commsys.com>
2025-01-17 17:02:16 -0500

    Added inventory functionality and naming functionality.

---
+++ Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public List<string> Inventory { get; set; } = new  List<string>();
+
+        public Player(string name) 
+        {
+            Name = name;            
+        }
+
+        public void AddItem(string item)
+        {
+            Inventory.Add(item);
+        }
+
+        public void ShowInventory()
+        {
+            Console.WriteLine("Your inventory contains:");
+            foreach (string item in Inventory)
+            {
+                Console.Write($"- {item}\r\n");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
-﻿namespace AdventureGame
+﻿using System.Numerics;
+
+namespace AdventureGame
 {
     internal class Program
     {
         private static bool isPlaying = true;
+
+        public static Player Player { get; private set; }
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to the Legend of Zelda Text Adventure Game!");
+        {            
+            Console.WriteLine("Welcome to the Legend of Zelda Text Adventure Game! Please enter your name");
+            string name = Console.ReadLine() ?? "Hero";
+            Player = new Player(name);
             Console.WriteLine("Your journey begins now...");
             Console.WriteLine();
 
             //bool isPlaying = true;
 
             while (isPlaying)
-            {
-                Console.Clear();
+            {                
                 Console.WriteLine("You are standing at your house in Kokiri Village. Where would you like to go?");
                 Console.WriteLine("1. Enter the Kokori forest");
                 Console.WriteLine("2. Explore the The Great Deku Tree");
@@ -34,7 +40,7 @@
                         break;
 
                     case "3":
-                        Console.WriteLine("You enter the shop and see a shield just sitting on one of the shelves, worth 40 rupees.");
+                        EnterKokiriShop();
                         break;
 
                     case "4":
@@ -69,6 +75,7 @@
                     break;
 
                 case "2":
+                    Console.Clear();
                     Console.WriteLine("You go back to the safety of your home.");
                     return;
 
@@ -133,6 +140,58 @@
 
                 case "2":
                     //Console.WriteLine("You go back to the safety of your home.");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+
+
+
+            }
+
+
+        }
+
+        static void EnterKokiriShop()
+        {
+            Console.Clear();
+            Console.WriteLine("You see 4 items on the shelf.");
+            Console.WriteLine("It is your lucky day, the shop is going out of business and everything is free.");
+            Console.WriteLine("The 4 items are as follows: Kokori Shield, Magic Potion, Deku Stick, Deku Seed ");
+            Console.WriteLine("Which item do you grab?");
+            Console.WriteLine("1. Kokori Shield");
+            Console.WriteLine("2. Magic Potion");
+            Console.WriteLine("3. Deku Stick");
+            Console.WriteLine("4. Deku Seed");
+
+            Console.WriteLine("Enter your choice");
+            string? choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("You Grab the Kokori Shield");
+                    Player.AddItem("Kokori Shield");
+                    Player.ShowInventory();
+                    return;
+
+                case "2":
+                    Console.WriteLine("You Grab the Magic Potion");
+                    Player.AddItem("Magic Potion");
+                    Player.ShowInventory();
+                    return;
+
+                case "3":
+                    Console.WriteLine("You Grab the Deku Stick");
+                    Player.AddItem("Deku Stick");
+                    Player.ShowInventory();
+                    return;
+
+                case "4":
+                    Console.WriteLine("You Grab the Deku Seed");
+                    Player.AddItem("Deku Seed");
+                    Player.ShowInventory();
                     return;
 
                 default:
